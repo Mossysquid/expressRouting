624188040061c465d19318bd74995bb4ffcdbbaf
Andrew Rasband <andrew.rasband@outlook.com>
2023-12-18 09:48:53 -0700

    trying to use render

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const port = 3005
+
+app.use(express.static('stuff'));
+app.post(/i*apple$/, (req, res)=>{
+    res.send('\nWe have an apple for you\n')
+})
+app.get('/coloe?u?r',(req, res ) =>{
+    res.setEncoding('\ngot you\n')
+});
+
+app.get(/i*fly$/, (req, res)=>{
+    res.send('\nIt is true\n')
+})
+app.route('banana')
+.get((req,res)=>{
+    res.send('get request/n')
+})
+.put((req, res)=>{
+    res.send('get put/n')
+})
+.delete((req, res)=>{
+    res.send('get delete/n')
+})
+app.listen(port, ()=>{
+    console.log(`on port ${port}`);
+});
