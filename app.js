96b463297c1d86338aefdb1c16a2489de85dd239
Andrew Rasband <andrew.rasband@outlook.com>
2023-12-18 10:12:17 -0700

    added index.html

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3005
+const port = process.env.PORT || 3005
 
 app.use(express.static('stuff'));
 app.post(/i*apple$/, (req, res)=>{
@@ -24,5 +24,5 @@ app.route('banana')
     res.send('get delete/n')
 })
 app.listen(port, ()=>{
-    console.log(`on port ${port}`);
+    console.log(`on port ${port}, ${process.env.PORT}`);
 });
